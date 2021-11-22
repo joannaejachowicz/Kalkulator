8e45756ee9b0a9459ca64487ab35b47e728659f4
Joanna Jachowicz <joanna.e.jachowicz@gmail.com>
2021-11-22 20:13:09 +0100

    FunkcjaSumowaniadodana

--- src/Operacje.java
+++ src/Operacje.java
@@ -1,2 +1,12 @@
 public class Operacje {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println(sumAsia(a,b));
+    }
+
+    public static int sumAsia(int a, int b) {
+        return (a+b);
+    }
 }
